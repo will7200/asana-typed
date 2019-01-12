7b80a32ed4845ec9d570169d0d155867de38bfb7
William Flores <williamfl@mccarran.com>
2019-01-11 17:21:56 -0800

    feat(query): add group by and comparision functionality

--- asana_typed/query.py
+++ asana_typed/query.py
@@ -1,3 +1,4 @@
+import re
 from operator import attrgetter
 from typing import List
 from functools import wraps, partial
@@ -25,6 +26,51 @@ def str_to_attrgetter(__function=None, classed=True, position=0):
         args = tuple(args)
         return __function(*args, **kwargs)
 
+    return f
+
+
+def classed_attrgetter(value: str, obj: object):
+    splitted = value.split('.')[::-1]
+    if len(splitted) <= 1:
+        return attrgetter(value)(obj)
+    rvalue = obj
+    while len(splitted) != 0:
+        lattr = attrgetter(splitted.pop())
+        rvalue = lattr(rvalue)
+    return rvalue
+
+
+def str_contains(attribute, pat, case=True, flags=0, regex=True):
+    """
+    Return lambda function whether given pattern/regex is
+    contained in each string.
+    Parameters
+    ----------
+    pat : string
+        Character sequence or regular expression
+    case : boolean, default True
+        If True, case sensitive
+    flags : int, default 0 (no flags)
+        re module flags, e.g. re.IGNORECASE
+    regex : bool, default True
+        If True use re.search, otherwise use Python in operator
+    Returns
+    """
+    if regex:
+        if not case:
+            flags |= re.IGNORECASE
+
+        regex = re.compile(pat, flags=flags)
+
+        f = lambda x: bool(regex.search(attribute(x)))
+    else:
+        if case:
+            f = lambda x: pat in attribute(x)
+        else:
+            upper_pat = pat.upper()
+            f = lambda x: upper_pat in attribute(x).upper()
+            # uppered = map(lambda x: x.upper(), arr)
+            # return f
     return f
 
 
@@ -63,6 +109,32 @@ class Query(object):
         view = self.get_list(True)
         self._view = list(view)
         return self._view
+
+    @str_to_attrgetter
+    def group_by(self, attribute: (attrgetter, str)):
+        item_list = self.get_list(clear=False)
+        group_map = {}
+        for item in item_list:
+            index = attribute(item)
+            items = group_map.get(index, [])
+            items.append(item)
+            group_map[index] = items
+        return group_map
+
+    @str_to_attrgetter
+    def equals(self, attribute: (attrgetter, str), value):
+        self._filters.append(lambda x: attribute(x) == value)
+        return self
+
+    @str_to_attrgetter
+    def not_equals(self, attribute: (attrgetter, str), value):
+        self._filters.append(lambda x: attribute(x) != value)
+        return self
+
+    @str_to_attrgetter
+    def contains(self, attribute: (attrgetter, str), value, **kwargs):
+        self._filters.append(str_contains(attribute, value, **kwargs))
+        return self
 
     @str_to_attrgetter
     def is_set(self, attribute: (attrgetter, str)):
